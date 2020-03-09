3e9d5c1be86f135a2a5e014afdaf6f3370b7c8ad
ghostedMonster <jose-antonio-1110@hotmail.com>
2020-03-09 19:52:11 +0100

    Updates

---
+++ precache-manifest.36808d78dc90ac383a867fd1cb26609b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f33b8ac566eb4b32e2dcfc53f975420",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b8bb73d384cc929f01e",
+    "url": "/static/css/main.dd438d72.chunk.css"
+  },
+  {
+    "revision": "21c1087b53ae7267500d",
+    "url": "/static/js/2.aa3c51a1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.aa3c51a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b8bb73d384cc929f01e",
+    "url": "/static/js/main.187ffed0.chunk.js"
+  },
+  {
+    "revision": "2b26b4b06b6197592734",
+    "url": "/static/js/runtime-main.7558282d.js"
+  }
+]);
