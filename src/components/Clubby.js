be66e12bb2cb2c75e7dc049348d4027d5ff618e5
ghostedMonster <jose-antonio-1110@hotmail.com>
2020-03-10 17:32:46 +0100

    Added video to the background

--- src/components/Clubby.js
+++ src/components/Clubby.js
@@ -2,19 +2,37 @@ import React from 'react'
 
 function Clubby(){
     const styles = {
-        marginTop: "15%",
-        marginBottom: "15%",
         backgroundColor: "#FA5151",
+        marginBottom: "20%",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        justifyContent: "center"
+        justifyContent: "center",
+        fontFamily: "Montserrat",
+        color: "white"
     }
+
     return(
         <div style={styles}>
-            <img style={{width: "60%", height: "auto"}} src='https://i.imgur.com/mVcN4QK.png' alt='Clubby' />
+            <video id="background-video" loop autoPlay>
+                <source src={require("./party.webm")} type="video/mp4" />
+                <source src={require("./party.webm")} type="video/ogg" />
+            </video>
+            <p style={{fontWeight: "bolder"}}>Imágenes cogidas de <a style={
+                {
+                    color: 'white'
+                }
+            } href="https://www.youtube.com/watch?v=8Xs6DZG0ouM">aquí</a> y <a style={
+                {
+                    color: 'white',
+
+                }
+            } href="https://www.youtube.com/watch?v=sMdGzAlQPbU">aquí</a>.</p>
         </div>
     )
 }
+
+//<img style={{width: "60%", height: "auto"}} src='https://i.imgur.com/mVcN4QK.png' alt='Clubby' />
+            
 
 export default Clubby
