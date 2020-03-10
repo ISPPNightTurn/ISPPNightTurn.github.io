69345ae297d6cafb3a03318c8db5bcd39a87755e
ghostedMonster <jose-antonio-1110@hotmail.com>
2020-03-10 18:43:09 +0100

    changed mockup images

--- src/components/Mockups.js
+++ src/components/Mockups.js
@@ -45,30 +45,30 @@ function Mockups(){
                     matches ? (
                         <div style={style}>
                         <div style={substyle}>
-                            <img style={{maxWidth: '60%', height: 'auto'}} src='https://i.imgur.com/PMKDt3n.png' alt='Eventos disponibles' />
+                            <img style={{maxWidth: '60%', height: 'auto'}} src={require("./images/Mockup4.png")} alt='Eventos disponibles' />
                             <h4>Revisa los eventos disponibles a tu alrededor</h4>
                         </div>
                         <div style={substyle}>
-                            <img  style={{maxWidth: '60%', height: 'auto'}} src='https://i.imgur.com/ELY1uwd.png' alt='Comprar entradas' />
+                            <img  style={{maxWidth: '60%', height: 'auto'}} src={require("./images/Mockup6.png")} alt='Comprar entradas' />
                             <h4>Compra tus entradas desde la misma aplicación</h4>
                         </div>
                         <div style={substyle}>
-                            <img  style={{maxWidth: '60%', height: 'auto'}} src='https://i.imgur.com/3GQ6ExC.png' alt='Comprar consumiciones' />
+                            <img  style={{maxWidth: '60%', height: 'auto'}} src={require("./images/Mockup7.png")} alt='Comprar consumiciones' />
                             <h4>¿No se enteran de lo que pides? Compra tus consumiciones desde la app</h4>
                         </div>
                         </div>
                     ) : (
                         <div style={style}>
                         <div style={substyleDesktop}>
-                            <img style={{maxWidth: '30%', height: 'auto'}} src='https://i.imgur.com/PMKDt3n.png' alt='Eventos disponibles' />
+                            <img style={{maxWidth: '30%', height: 'auto'}} src={require("./images/Mockup4.png")} alt='Eventos disponibles' />
                             <h4>Revisa los eventos disponibles a tu alrededor</h4>
                         </div>
                         <div style={substyleDesktop}>
-                            <img  style={{maxWidth: '30%', height: 'auto'}} src='https://i.imgur.com/ELY1uwd.png' alt='Comprar entradas' />
+                            <img  style={{maxWidth: '30%', height: 'auto'}} src={require("./images/Mockup6.png")} alt='Comprar entradas' />
                             <h4>Compra tus entradas desde la misma aplicación</h4>
                         </div>
                         <div style={substyleDesktop}>
-                            <img  style={{maxWidth: '30%', height: 'auto'}} src='https://i.imgur.com/3GQ6ExC.png' alt='Comprar consumiciones' />
+                            <img  style={{maxWidth: '30%', height: 'auto'}} src={require("./images/Mockup7.png")} alt='Comprar consumiciones' />
                             <h4>¿No se enteran de lo que pides? Compra tus consumiciones desde la app</h4>
                         </div>
                         </div>
